ee721550c85b715fcd3b792c8a813ea4ba612a3d
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-02-06 23:33:06 -0300

    log

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -4,8 +4,6 @@ import jwtGenerate from './jwtGenerate';
 
 export default async function listOrders(login: ILogin) {
   const user = await LoginModel.default(login);
-  console.log(user);
-  
   if (user.length === 0) return { type: 401, message: { message: 'Username or password invalid' } };
   return { type: 200, message: { token: jwtGenerate(user as unknown as IUser) } };
 }
