d283a148d3d425cb1475db2b0ff46853885a0134
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-02-08 10:19:55 -0300

    feat: refat middlewares para class

--- src/routers/ordersRouter.ts
+++ src/routers/ordersRouter.ts
@@ -4,6 +4,7 @@ import OrdersModel from '../models/OrdersModel';
 import connection from '../models/connection';
 import OrdersService from '../services/OrdersService';
 import ProductsModel from '../models/ProductsModel';
+import JwtValidate from '../middlewares/JwtValidate';
 
 const ordersRouter = Router();
 
@@ -11,7 +12,9 @@ const ordersModel = new OrdersModel(connection);
 const productsOrders = new ProductsModel(connection);
 const ordersService = new OrdersService(ordersModel, productsOrders);
 const ordersController = new OrdersController(ordersService);
+const jwtValidate = new JwtValidate();
 
 ordersRouter.get('/', ordersController.listOrders);
+ordersRouter.post('/', jwtValidate.auth, ordersController.listOrders);
 
 export default ordersRouter;
